ccef8757cab267944380370a075547da5c710b9c
Beandip <brando.lugo@gmail.com>
2021-03-25 21:25:49 -0500

    Addedd basic connections to the database to add and update inventory it needs work

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 
 //Local imports
 import 'package:qr_inventory/services/auth.dart';
+import 'package:qr_inventory/services/database.dart';
+import 'package:qr_inventory/models/inventoryItem.dart';
 
 class Home extends StatefulWidget {
   final FirebaseAuth auth;
@@ -16,6 +18,8 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final TextEditingController _inventoryController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,6 +31,68 @@ class _HomeState extends State<Home> {
               onPressed: () {
                 Auth(auth: widget.auth).signOut();
               })
+        ],
+      ),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 20,
+          ),
+          const Text("Add Inventory Here: "),
+          TextFormField(
+            controller: _inventoryController,
+          ),
+          IconButton(
+            onPressed: () {
+              if (_inventoryController.text != "") {
+                setState(() {
+                  Database(firestore: widget.firestore).addInventoryItem(
+                    uid: widget.auth.currentUser.uid,
+                    props: {_inventoryController.text: "item"},
+                  );
+                  _inventoryController.clear();
+                });
+              }
+            },
+            icon: const Icon(Icons.add),
+          ),
+          const Text("Inventory List"),
+          Expanded(
+              child: StreamBuilder(
+                  stream: Database(firestore: widget.firestore)
+                      .streamInventory(uid: widget.auth.currentUser.uid),
+                  builder: (BuildContext context,
+                      AsyncSnapshot<List<InventoryItem>> snapshot) {
+                    if (snapshot.connectionState == ConnectionState.active) {
+                      if (snapshot.data.isEmpty) {
+                        return const Center(
+                          child: Text("Empty"),
+                        );
+                      }
+
+                      return ListView.builder(
+                        itemCount: snapshot.data.length,
+                        itemBuilder: (_, index) {
+                          return Card(
+                            child: Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Row(
+                                children: [
+                                  Expanded(
+                                      child:
+                                          Text(snapshot.data[index].itemName))
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    } else {
+                      return const Center(
+                        child: Text("Loading..."),
+                      );
+                    }
+                  }))
         ],
       ),
     );
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,16 +1,60 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:qr_inventory/models/inventory.dart';
+import 'package:qr_inventory/models/inventoryItem.dart';
 
 class Database {
   final FirebaseFirestore firestore;
 
-  Database(this.firestore);
+  Database({this.firestore});
 
-  Stream<Inventory> streamInventory({String uid}){
-    try{
-      return firestore.collection("inventory").doc(uid).collection("inventoryItems").snapshots()
+  Stream<List<InventoryItem>> streamInventory({String uid}) {
+    try {
+      return firestore
+          .collection("inventory")
+          .doc(uid)
+          .collection("inventoryItems")
+          .snapshots()
+          .map((query) {
+        List<InventoryItem> retVal;
+
+        for (final DocumentSnapshot doc in query.docs) {
+          retVal.add(
+            InventoryItem.fromDocumentSnapshots(documentSnapshot: doc),
+          );
+        }
+        return retVal;
+      });
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<void> addInventoryItem(
+      {String uid, String itemName, Map<String, String> props}) async {
+    try {
+      firestore
+          .collection("inventory")
+          .doc(uid)
+          .collection("inventoryItems")
+          .add({
+        "name": itemName,
+        "properties": props,
+      });
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<void> updateInventoryItem(
+      {String uid, String itemId, Map<String, String> props}) async {
+    try {
+      firestore
+          .collection("inventory")
+          .doc(uid)
+          .collection("inventoryItems")
+          .doc(itemId)
+          .update({
+        "properties": props,
+      });
     } catch (e) {
       rethrow;
     }
